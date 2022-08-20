d0848e1326d59a093a310614132af99a21ce53fa
andropyro28 <menandroeugenio1028@gmail.com>
2022-08-20 03:59:10 -0400

    ewan

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,12 +2,14 @@ const mysql = require("mysql2");
 require('dotenv').config();
 
 const mysqlDB = mysql.createPool({
+
     multipleStatements: true,
     host: process.env.DB_HOST_PROD,
     user: process.env.DB_USER_PROD,
     password: process.env.DB_PASSWORD_PROD,
     database: process.env.DB_NAME_PROD,
-    connectionLimit: 5
+    connectionLimit: 5,
+
 })
 
 module.exports = mysqlDB.promise()
