0e9672ba1e2ee9f750d9d0321191f532f92c993b
Menandro Eugenio <menandroeugenio1028@gmail.com>
2022-08-27 16:23:20 +0800

    ...

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.get('/api/auth', verifyUser, (req, res) => {
     }
 })
 
-server.listen(PORT, () => console.info(`server listening on port ${PORT}`)) // fuck this
-
+server.listen(PORT, () => console.info(`server listening on port ${PORT}`)) 
 const io = new Server(server, { // we will use this later
     cors: {
         origin: "*",
