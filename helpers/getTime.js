dbf6a9b9936610233b08c59c5329ffda1598e0dd
Menandro Eugenio <menandroeugenio1028@gmail.com>
2022-08-27 16:20:38 +0800

    ..

--- helpers/getTime.js
+++ helpers/getTime.js
@@ -4,7 +4,7 @@ const getTime = () => {
     const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
     const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
     const ampm = hours >= 12 ? 'pm' : 'am';
-    return `${hours}:${minutes} ${ampm}`
+    return `${hours % 12}:${minutes} ${ampm}`
 }
 
 module.exports = getTime
