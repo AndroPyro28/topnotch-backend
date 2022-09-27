41008b7ad3dae08ac3c2806c5c0e4f87189b7255
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-27 17:48:22 +0800

    'update'

--- models/Category.js
+++ models/Category.js
@@ -18,8 +18,23 @@ class Category {
     }
   };
 
+  getCategoryByCategoryName = async () => {
+    try {
+      const selectQuery = `SELECT id as category_id FROM product_category WHERE category = ?;`
+      const [result, _ ] = await  poolConnection.execute(selectQuery, [this.#category])
+      return result;
+    } catch (error) {
+      console.error('here error', error.message)
+    }
+  }
+
   addCategory = async () => {
     try {
+      const queryResult = await this.getCategoryByCategoryName();
+      const {category_id} = queryResult[0]
+      if(category_id) {
+        throw new Error('category is alredy exist');
+      }
       const insertQuery = `INSERT INTO product_category (category) VALUES (?);`
       const [result, _ ] = poolConnection.execute(insertQuery, [this.#category]);
       return result;
