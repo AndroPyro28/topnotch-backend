cc9ad64bfc986d5fef29da0078fbb4f1c6bd3493
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-28 17:13:47 +0800

    'update'

--- models/Order.js
+++ models/Order.js
@@ -83,7 +83,8 @@ class Order {
        ON c.id = od.customer_id
        WHERE ${
          this.#order_status == "all"
-           ? `od.order_status LIKE ? OR od.order_status LIKE ?`
+           ? `od.order_status LIKE ? 
+           OR od.order_status LIKE ?`
            : `od.order_status LIKE ?`
        } AND
        od.reference LIKE ?
