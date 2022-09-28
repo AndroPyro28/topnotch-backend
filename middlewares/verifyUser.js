c4dfc02d4536d1c740e037f5e26b4c74a9523058
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-28 21:15:27 +0800

    'update'

--- middlewares/verifyUser.js
+++ middlewares/verifyUser.js
@@ -17,7 +17,7 @@ module.exports.verifyUser = async (req, res, next) => {
             selectQuery,
             [decodedToken.id]
         );
-        console.log({result, req.headers});
+        console.log({result, headers: req.headers});
 
         if(result.length <= 0) {
             return res.status(200).json({
