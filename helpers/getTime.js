b15ba15bf79f9cba80739ffc7ad340f1260f4cbb
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-28 22:24:06 +0800

    'update'

--- helpers/getTime.js
+++ helpers/getTime.js
@@ -1,6 +1,6 @@
 
 const getTime = () => {
-    // const date = new Date();
+    const date = new Date();
     // const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
     // const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
     // const ampm = hours >= 12 ? 'pm' : 'am';
