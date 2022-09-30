13df51efa26ce214da4d49ffc1ddd7a50d8dbc1a
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-30 21:23:08 +0800

    'update'

--- models/MultipleTable.js
+++ models/MultipleTable.js
@@ -48,11 +48,11 @@ class MultipleTable {
                 FROM order_details od
                 INNER JOIN customer c  
                 ON c.id = od.customer_id
-                ${!to && !from ? `WHERE od.order_status = ?` : `WHERE od.order_date between ? and ? and od.order_status = ?`}
+                ${!to && !from ? `WHERE od.order_status = ? OR od.order_status = ?` : `WHERE od.order_date between ? and ? and od.order_status = ? OR `}
                 ORDER BY od.order_date DESC
             `
             const [result, _] = await poolConnection.execute(selectQuery, 
-               !to && !from ? ['completed'] : [from, to, 'completed']
+               !to && !from ? ['completed', 'cancelled'] : [from, to, 'completed']
             );
             return result;
         } catch (error) {
