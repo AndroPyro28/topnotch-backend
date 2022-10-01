d062ffaf1ad53e772bff59b1630650e11806363a
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-01 13:43:43 +0800

    'update'

--- helpers/FormatDatelocal.js
+++ helpers/FormatDatelocal.js
@@ -1,7 +1,7 @@
 
 function FormateDateLocal(date) {
     console.log('formatting', date);
-    const newDate =  date.substring(0,date.indexOf("+") - 5).replace("T", " ");
+    const newDate =  date.substring(0,date.indexOf("+")).replace("T", " ");
     return newDate
 }
 
