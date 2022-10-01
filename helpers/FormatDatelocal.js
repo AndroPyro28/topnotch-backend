6e67250205e9de755169ffac2a6742062b254864
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-01 14:23:34 +0800

    'update'

--- helpers/FormatDatelocal.js
+++ helpers/FormatDatelocal.js
@@ -1,8 +1,8 @@
 
 function FormateDateLocal(date) {
     console.log('formatting', date);
-    const newDate =  date.substring(0,date.indexOf("+")).replace("T", " ") + ".000000";
-    return newDate
+    const newDate =  date.substring(0,date.indexOf("+")).replace("T", " ") ;
+    return `${newDate}.000000`
 }
 
 module.exports = FormateDateLocal
