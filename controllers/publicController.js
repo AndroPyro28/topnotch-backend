458f278d486496ffb5c2313da43f0b44c348f598
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-02 15:16:11 +0800

    'update'

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -4,6 +4,7 @@ module.exports.getFirstThreeFeedback = async (req, res) => {
     try {
         const feedbackModel = new Feedback({});
         const result = await feedbackModel.getFirstThreeFeedback();
+        console.log(result);
         return result;
     } catch (error) {
         console.error(error.message)
