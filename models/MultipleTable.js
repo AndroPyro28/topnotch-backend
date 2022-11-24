1df4dd411d48bb5696b3bd44d9324d39826b9503
Andropyro28 <menandroeugenio1028@gmail.com>
2022-11-24 15:00:07 +0800

    'update'

--- models/MultipleTable.js
+++ models/MultipleTable.js
@@ -139,7 +139,7 @@ class MultipleTable {
 
   getPinnedEmployees = async () => {
     try {
-      const selectQuery = `SELECT * from admin WHERE pin = ?`;
+      const selectQuery = `SELECT admin.id, admin.firstname, admin.lastname, admin.profile_image_url from admin WHERE pin = ?`;
       const [result, _] = await poolConnection.query(selectQuery, [true]);
       return result;
     } catch (error) {
